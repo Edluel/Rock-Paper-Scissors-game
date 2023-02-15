a8a2ba328aacfb35885e9d1937f329f7fd2a75e7
Edluel <edluel_machado@hotmail.com>
2023-02-14 23:17:08 -0300

    function to change choice screen to load screen

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,9 @@ function toggleRules() {
     rules.style.display === 'none' ? rules.style.display = 'block' : rules.style.display = 'none';
 }
   
+function choice(choice) {
+    const userchoice = choice;
+    document.querySelector('.choice').style.display = 'none';
+    document.querySelector('.load').style.display = 'flex';
+
+}
